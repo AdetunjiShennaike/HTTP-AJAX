53ab4784190aa8bae50de8f22eeec01764615e25
Adetunji <ashennaike@gmail.com>
2019-04-03 17:45:18 -0400

    got data and loading spinner

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import Friends from './components/FriendsList';
+import Loader from 'react-loader-spinner'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      friends: []
+    }
+  }
+
+
+  componentDidMount() {
+    //ajax command to get information form the server
+    axios.get('http://localhost:5000/friends')
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+    this.setState({
+
+    })
+  }
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-         
-        </header>
-      </div>
-    );
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className='title'>Here's all the friends you have.</h1>
+            <h3 className='joke'>Which is not a lot...</h3>         
+          </header>
+
+          {this.state.friends.length === 0 
+          ? 
+            <Loader className='loading'
+              type="Triangle"
+              color="#00BFFF"
+              height="100"	
+              width="100"
+            />
+          :
+            this.state.friends.map( event => 
+            <Friends key={event.id} friends={this.state.friends}/>
+            )
+          }
+        </div>
+      );
   }
 }
 
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 
-let Friends = () => {
+let Friends = (props) => {
   return (
   
     <div className='list'>
-      
+      <p>{props.friends.name}</p>
+      <p>{props.friends.age}</p>
+      <p>{props.friends.email}</p>
+      <p>{props.friends.id}</p>
+
     </div>
   )  
 }
