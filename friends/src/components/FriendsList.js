db3fa5f7ff63c1eafa956f0763fd842f1b3a545d
Adetunji <ashennaike@gmail.com>
2019-04-03 19:01:01 -0400

    fixed spelling

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+//styling the friend card for the list
+let Card = styled.div`
+  display: flex;
+`
+
+
 
 
 let Friends = (props) => {
   return (
   
     <div className='list'>
+      <p>{props.friends.id}:</p>
       <p>{props.friends.name}</p>
       <p>{props.friends.age}</p>
       <p>{props.friends.email}</p>
-      <p>{props.friends.id}</p>
 
     </div>
   )  
